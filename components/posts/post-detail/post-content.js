6c7676ea520fb0ff081ca39ce1ec4aa7690c0b3f
zana-abdollahpour <zana.ap4@gmail.com>
2023-04-06 03:53:18 +0430

    optimize images in markdown with Image element from nextjs

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactMarkdown from "react-markdown";
+import Image from "next/image";
 
 import styles from "./post-content.module.css";
 import PostHeader from "./post-header";
@@ -9,10 +10,34 @@ export default function PostContent(props) {
 
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
+  const customComponents = {
+    p: (paragraph) => {
+      const { node } = paragraph;
+
+      if (node.children[0].tagName === "img") {
+        const image = node.children[0];
+
+        return (
+          <div className={styles.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.properties.src}`}
+              width={600}
+              height={300}
+              alt={image.alt}
+            />
+          </div>
+        );
+      }
+      return <p>{paragraph.children}</p>;
+    },
+  };
+
   return (
     <article className={styles.content}>
       <PostHeader title={post.title} image={imagePath} />
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <ReactMarkdown components={customComponents}>
+        {post.content}
+      </ReactMarkdown>
     </article>
   );
 }
