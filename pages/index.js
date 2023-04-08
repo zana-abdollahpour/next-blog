ae01be53ae265fcc9b876c0e7bb66cabbd705e85
zana-abdollahpour <zana.ap4@gmail.com>
2023-04-08 16:53:14 +0430

    fix a typo in hero component import

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Head from "next/head";
 
-import Hero from "../components/home-page/Hero";
+import Hero from "../components/home-page/hero";
 import FeaturedPosts from "../components/home-page/featured-posts";
 import { getFeaturedPosts } from "../lib/posts-util";
 
